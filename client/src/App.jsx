ef2a07f78d2d56efb556eee8c573ec12a98d9f24
Aloka Devapriya <28234480+Alokadev@users.noreply.github.com>
2023-11-24 18:10:59 +0530

    Header Developed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/Signup';
 import About from './pages/about';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/sign-in' element={<SignIn />}/>
